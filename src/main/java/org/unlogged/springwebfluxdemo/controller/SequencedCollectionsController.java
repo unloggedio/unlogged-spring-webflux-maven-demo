fc8b380bf9f7c2ea792ed8a60ae1d29e19a8e8e1
Akshat Jain <akshatjain9568@gmail.com>
2024-05-09 16:11:33 +0530

    Covering cases for SequencedCollectionsController, adding 9 replay cases for the same

--- src/main/java/org/unlogged/springwebfluxdemo/controller/SequencedCollectionsController.java
+++ src/main/java/org/unlogged/springwebfluxdemo/controller/SequencedCollectionsController.java
@@ -19,7 +19,9 @@ public class SequencedCollectionsController {
                     seq.add("d");
                     seq.addLast("e");
                     System.out.println(seq.getLast());
+                    System.out.println(seq.getFirst());
                     seq.removeFirst();
+                    seq.removeLast();
                     return seq.reversed();
                 });
     }
@@ -34,7 +36,9 @@ public class SequencedCollectionsController {
                     seq.add("d");
                     seq.addLast("e");
                     System.out.println(seq.getLast());
+                    System.out.println(seq.getFirst());
                     seq.removeFirst();
+                    seq.removeLast();
                     return seq.reversed();
                 });
     }
@@ -48,9 +52,79 @@ public class SequencedCollectionsController {
                 .map(mp -> {
                     mp.putFirst(1,"a");
                     mp.putLast(4,"d");
-                    System.out.println(mp.lastEntry().getKey()+":"+mp.lastEntry().getValue());
+                    System.out.println("Last Entry: "+mp.lastEntry().getKey()+":"+mp.lastEntry().getValue());
+                    System.out.println("First Entry: "+mp.firstEntry().getKey()+":"+mp.firstEntry().getValue());
                     mp.pollLastEntry();
+                    mp.pollFirstEntry();
                     return mp.reversed();
+                });
+    }
+
+    @RequestMapping("/sequence-map/keyset")
+    public Mono<SequencedSet<Integer>> seqKeySetFromMap() {
+        SequencedMap<Integer, String> sequencedMap = new LinkedHashMap<>();
+        sequencedMap.put(2, "b");
+        sequencedMap.put(3, "c");
+        return Mono.just(sequencedMap.reversed().sequencedKeySet());
+    }
+
+    @RequestMapping("/sequence-map/valueset")
+    public Mono<SequencedCollection<String>> seqValueSetFromMap() {
+        SequencedMap<Integer, String> sequencedMap = new LinkedHashMap<>();
+        sequencedMap.put(2, "b");
+        sequencedMap.put(3, "c");
+        return Mono.just(sequencedMap.reversed().sequencedValues());
+    }
+
+    @RequestMapping("/sequence-map/entryset")
+    public Mono<SequencedSet<Map.Entry<Integer, String>>> seqEntrySetFromMap() {
+        SequencedMap<Integer, String> sequencedMap = new LinkedHashMap<>();
+        sequencedMap.put(2, "b");
+        sequencedMap.put(3, "c");
+        return Mono.just(sequencedMap.reversed().sequencedEntrySet());
+    }
+
+    // Should throw UnsupportedOperationException
+    @RequestMapping("/sequence-map/unmod")
+    public Mono<SequencedMap<Integer, String>> seqMapUnModifiable() {
+        SequencedMap<Integer, String> sequencedMap = new LinkedHashMap<>();
+        sequencedMap.put(2, "b");
+        sequencedMap.put(3, "c");
+        SequencedMap<Integer, String> unmodSequencedMap = Collections.unmodifiableSequencedMap(sequencedMap);
+        return Mono.just(unmodSequencedMap)
+                .map(mp -> {
+                    mp.putFirst(1,"a");
+                    return mp;
+                });
+    }
+
+    @RequestMapping("/sequence-list/unmod")
+    public Mono<SequencedCollection<String>> seqListUnmodifiable() {
+        SequencedCollection<String> sequencedCollection = new ArrayList<>();
+        sequencedCollection.addFirst("ab");
+        sequencedCollection.add("c");
+        sequencedCollection.add("d");
+        sequencedCollection.addLast("e");
+        SequencedCollection<String> unmodSequencedCollection = Collections.unmodifiableSequencedCollection(sequencedCollection);
+        return Mono.just(unmodSequencedCollection)
+                .map(seq -> {
+                    seq.addFirst("xyz");
+                    return seq;
+                });
+    }
+
+    @RequestMapping("/sequence-set/unmod")
+    public Mono<SequencedSet<String>> seqSetUnmodifiable() {
+        SequencedSet<String> sequencedSet = new LinkedHashSet<>();
+        sequencedSet.addFirst("ab");
+        sequencedSet.add("c");
+        sequencedSet.add("d");
+        sequencedSet.addLast("e");
+        SequencedSet<String> unmodSeqSet = Collections.unmodifiableSequencedSet(sequencedSet);
+        return Mono.just(unmodSeqSet)
+                .map(seq -> {
+                    seq.addFirst("xyz");
+                    return seq;
                 });
     }
 }
