f11116fdd72f30a8779880f52ae680161d6e4b37
Akshat Jain <akshatjain9568@gmail.com>
2024-06-07 17:18:17 +0530

    getting reactive working (RedisOpsController)

--- src/main/java/org/unlogged/springwebfluxdemo/controller/RedisOpsController.java
+++ src/main/java/org/unlogged/springwebfluxdemo/controller/RedisOpsController.java
@@ -1,25 +1,42 @@
 package org.unlogged.springwebfluxdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.core.ReactiveValueOperations;
 import org.unlogged.springwebfluxdemo.model.EmployeeV1;
 import reactor.core.publisher.Mono;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+@RestController
 public class RedisOpsController {
 
-    @Autowired
-    private ReactiveRedisTemplate<String, EmployeeV1> redisTemplate;
-    private ReactiveValueOperations<String, EmployeeV1> reactiveValueOps;
+    private final ReactiveRedisOperations<String, String> redisOps;
+    private final ObjectMapper objectMapper;
 
-    public Mono<EmployeeV1> redisOps1() throws InterruptedException {
-        reactiveValueOps = redisTemplate.opsForValue();
-        Mono<Boolean> result = reactiveValueOps.set("123",
-                new EmployeeV1("123", "Bill", "Accounts"));
-//        Thread.sleep(200);
-        Mono<EmployeeV1> fetchedEmployee = reactiveValueOps.get("123");
-//        Thread.sleep(200);
-        return fetchedEmployee;
+    @Autowired
+    public RedisOpsController(ReactiveRedisOperations<String, String> redisOps, ObjectMapper objectMapper) {
+        this.redisOps = redisOps;
+        this.objectMapper = objectMapper;
     }
 
+    @RequestMapping("/redis/set")
+    public Mono<Boolean> setValue(@RequestBody EmployeeV1 employeeV1) {
+        return redisOps.opsForValue().set(employeeV1.getId(), employeeV1.toJson());
+    }
+
+    @GetMapping("/redis/get/{key}")
+    public Mono<EmployeeV1> getValue(@PathVariable String key) {
+        return redisOps.opsForValue().get(key)
+                .flatMap(json -> {
+                    try {
+                        return Mono.justOrEmpty(objectMapper.readValue(json, EmployeeV1.class));
+                    } catch (Exception e) {
+                        return Mono.empty();
+                    }
+                });
+    }
 }
