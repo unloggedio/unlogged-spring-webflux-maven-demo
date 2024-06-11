db47c52845da5389a7dbefdf7c05299c6eaea60d
Akshat Jain <akshatjain9568@gmail.com>
2024-06-11 10:20:13 +0530

    Added sql crud

--- src/main/java/org/unlogged/springwebfluxdemo/model/Player.java
+++ src/main/java/org/unlogged/springwebfluxdemo/model/Player.java
@@ -1,11 +1,54 @@
 package org.unlogged.springwebfluxdemo.model;
 
-import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
-@Data
+@Table(name = "players")
 public class Player {
+    @Id
     private Long id;
     private String name;
     private int age;
-    private String team;
+
+    public Player(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    public Player() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
